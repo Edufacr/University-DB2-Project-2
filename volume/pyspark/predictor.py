b5907c218cfa1d68b96c5c6e6dc3178633beef52
Eduardo <edumadrim@gmail.com>
2021-01-30 22:50:54 -0600

    json format

--- volume/pyspark/predictor.py
+++ volume/pyspark/predictor.py
@@ -1,10 +1,11 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, date_format, to_date, year
+from pyspark.sql.functions import col, date_format, to_date, year, collect_list, struct, lit
 from pyspark.sql.types import (DateType, IntegerType, FloatType, StructField,
                                StructType, TimestampType, StringType)
 
 
 spark = SparkSession.builder.appName('Read Transactions').getOrCreate()
+outputPath = 'hdfs://127.0.0.1:9000/data/output/pyspark'
 
 '''
 MapR Output: team_name, player_name, birth_date, current_price, price_change
@@ -23,19 +24,42 @@ player_schema = StructType([StructField('name', StringType()),
                             StructField('potential_change', FloatType()),
                             StructField('overall_change', FloatType())])
 
+# predictions_schema = StructType([StructField('team_name',StringType()),
+#                                  StructField('player',
+#                                     StructField('player_name',StringType()),
+#                                      StructField('current_price',IntegerType()),
+#                                      StructField('predicted_value',FloatType()))])
+
 try:
     # creates initial data frames
     market = spark.read.csv('hdfs://127.0.0.1:9000/data/output/mapr/part-r-00000', schema=market_schema, ignoreTrailingWhiteSpace=True)
     player = spark.read.csv('hdfs://127.0.0.1:9000/data/output/hive/000000_0', schema=player_schema)
 
     # joins both dataframes and selects necessary info
-    # current_price is left temporarily, for development purposes
     join_condition = (market.player_name==player.name) & (market.birth_date==player.bdate)
+
+    # current_price is left temporarily, for development purposes
+    # predictions = market.join(player,join_condition,'inner').selectExpr(
+    #                     "team_name", "player_name", "current_price", "(((overall_change+price_change+2)/2)+potential_change)*current_price AS predicted_value")
     predictions = market.join(player,join_condition,'inner').selectExpr(
-                        "team_name", "player_name", "current_price", "(((overall_change+price_change+2)/2)+potential_change)*current_price AS predicted_value")
+                    "team_name", "player_name", "(((overall_change+price_change+2)/2)+potential_change)*current_price AS predicted_value")
     
-    predictions.show(100)
-    predictions.write.mode("overwrite").json('hdfs://127.0.0.1:9000/data/output/pyspark')
+    predictionsFormatted = predictions.withColumn('player',struct(
+                                                                col("player_name").alias("name"),
+                                                                col("predicted_value").alias("value"))
+                                                                    ).drop("predicted_value","player_name")
+
+    playersByGroup = predictionsFormatted.groupBy("team_name").agg(collect_list("player").alias("Players"))
+    playersByGroup = playersByGroup.withColumn("name",lit("England"))
+    playersByGroup = playersByGroup.withColumn("club", struct(
+                                                                col("team_name").alias("name"),
+                                                                col("Players").alias("children"))
+                                                                ).drop("team_name","Players")
+
+    playersByGroup = playersByGroup.groupBy("name").agg(collect_list("club").alias("children"))
+    #playersByGroup.show(100)
+    playersByGroup.write.mode("overwrite").json(outputPath)
+    #playersByGroup.printSchema()
 
 except Exception as e:
     print(e)
